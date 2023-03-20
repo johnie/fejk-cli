709139b596142439c66134006fc59e4b9941fcc3
Johnie Hjelm <johnie@bambuser.com>
2023-03-20 13:07:49 +0100

    tests: Update tests

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ import test from 'ava';
 import { execa } from 'execa';
 
 test('main', async (t) => {
-  const fejk = await execa('./cli.js', ['--table']);
+  const fejk = await execa('./cli.js', ['--raw']);
   t.true(fejk.stdout.length > 0);
 });
